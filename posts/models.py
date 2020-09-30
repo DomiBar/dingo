3d30f194bf71b8e44f59f4c38d5d2dcca355a8d4
Dominik Baran <dominik.baran1992@gmail.com>
2020-09-30 10:44:18 +0200

    Update models in posts app

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
 
-DEFAULT_AUTHOR_ID = 999
-
-
 class Post(models.Model):
     title = models.CharField(max_length=20)
     content = models.TextField()
@@ -11,14 +8,11 @@ class Post(models.Model):
     modified = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(
         "posts.Author",
-        on_delete=models.CASCADE,
-        default=DEFAULT_AUTHOR_ID
+        on_delete=models.CASCADE
     )
 
     def __str__(self):
         return f"{self.author}, {self.title}"
-
-
 
 
 class Author(models.Model):
