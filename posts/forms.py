427267aea40747433b0e817e4575d87c8dd8cce8
Dominik Baran <dominik.baran1992@gmail.com>
2020-10-06 06:55:56 +0200

    Add requirements

--- posts/forms.py
+++ posts/forms.py
@@ -10,19 +10,8 @@ class PostForm(forms.Form):
    title = forms.CharField()
    content = forms.CharField(widget=forms.Textarea)
 
-   def clean(self):
-       cleaned_data = super().clean()
-       author = cleaned_data.get('author')
-       title = cleaned_data.get('title')
-       content = cleaned_data.get('content')
 
 class AuthorForm(forms.Form):
    nick = forms.CharField()
    email = forms.EmailField()
    bio = forms.CharField(required=False,widget=forms.Textarea)
-
-   def clean(self):
-       cleaned_data = super().clean()
-       author = cleaned_data.get('author')
-       title = cleaned_data.get('title')
-       content = cleaned_data.get('content')
